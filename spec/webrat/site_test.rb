006300397a9da99637520c8089a3060dcbfd44de
roylez <roylzuo@gmail.com>
2012-07-21 20:29:53 +0800

    remove useless webrat test

--- spec/webrat/site_test.rb
+++
@@ -1,9 +0,0 @@
-require "stories_helper"
-
-describe 'site' do
-  it "should show me the homepage so I know Monk is correctly installed" do
-    visit "/"
-
-    body.should contain('Hello, world!')
-  end
-end
