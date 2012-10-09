38a6e305cd82405127a8547366451d71f164ea29
roylez <roylzuo@gmail.com>
2012-10-09 12:00:23 +0800

    minor change to sorting algorithm to lower the impact of time for new posts

--- app/models/sorting.rb
+++ app/models/sorting.rb
@@ -5,16 +5,37 @@
 #     http://amix.dk/blog/post/19588
 
 # hot topics
+#   original reddit sorting   log10(z) + y * t /12.5h
+#def post_hot_score(up, down, time)
+  ## 1249027200
+  #karma = up - down
+  #order = Math.log10([karma.abs, 1].max)
+  #sign = ( karma > 0 ? 1 : -1 )
+  #sign = 0  if karma == 0
+  #seconds = time.to_i - 1249027200
+
+  #(order + sign * seconds / 45000.0).round(7)
+#end
+
+# new hot topic sorting
+#   the idea is that a yesterday's post with 10 upvotes should be better than a
+#   today's post with 3 votes, nature logrithm is used insead of base 10
 #
+#     log(10) - log(3) > 24h/b =>  b = 20h
+#
+#   the equation is thus like
+#
+#     log(z) + y * t / 20h
+#   
 def post_hot_score(up, down, time)
   # 1249027200
   karma = up - down
-  order = Math.log10([karma.abs, 1].max)
+  order = Math.log([karma.abs, 1].max)
   sign = ( karma > 0 ? 1 : -1 )
   sign = 0  if karma == 0
   seconds = time.to_i - 1249027200
 
-  (order + sign * seconds / 45000.0).round(7)
+  (order + sign * seconds / 72000.0).round(7)
 end
 
 # controversial topics
