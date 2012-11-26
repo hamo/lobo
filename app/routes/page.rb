e8bd8789c2be095403e0a0f3630426a95562630d
roylez <roylzuo@gmail.com>
2012-11-26 14:48:55 +0800

    change brand for several pages

--- app/routes/page.rb
+++ app/routes/page.rb
@@ -10,24 +10,28 @@ class Main
 
   get "/" do
     @title = '主页'
+    @brand = '嘟噜'
     @posts = paginate_posts(viewable_posts, :sort_by => :score, :order => 'DESC')
     haml :post_list
   end
 
   get "/latest" do
     @title = '最新'
+    @brand = '嘟噜'
     @posts = paginate_posts(all_viewable_posts, :sort_by => :created_at, :order => 'DESC')
     haml :post_list
   end
 
   get '/controversial' do
     @title = '最具争议'
+    @brand = '嘟噜'
     @posts = paginate_posts(viewable_posts, :sort_by => :controversy, :order => 'DESC')
     haml :post_list
   end
 
   get '/top' do
     @title = '分数最高'
+    @brand = '嘟噜'
     @posts = paginate_posts(viewable_posts, :sort_by => :karma, :order => 'DESC')
     haml :post_list
   end
@@ -42,6 +46,7 @@ class Main
 
   get '/browser' do
     @title = "请升级您的浏览器"
+    @brand = '嘟噜'
     haml :browser, :layout => false, :format => :xhtml
   end
 
