950e559ccbf74f7916ba575e1b35d469a40b9e9f
roylez <roylzuo@gmail.com>
2013-01-20 21:56:32 +0800

    fix for post detail page is_new_reply? error

--- app/helpers/comment.rb
+++ app/helpers/comment.rb
@@ -13,7 +13,8 @@ module LoboHelpers
     # is current comment a new reply for current user?
     def is_new_reply?(comment)
       return false unless logged_in?
-      return false unless @unread_in_thread or @unread_in_thread.empty?
+      return false unless @unread_in_thread
+      return false if @unread_in_thread.empty?
       @unread_in_thread.collect(&:last).flatten.include? comment.id
     end
 
